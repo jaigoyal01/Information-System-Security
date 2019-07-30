446a11c908e6f0f88455d83117932c7f9027e570
Jayansh Goyal <33573720+smartjayansh@users.noreply.github.com>
2019-07-30 19:11:04 +0530

    Added cipher program

---
+++ CaesarCipher.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+string encp(string text,int key)
+{
+
+	if(key>26)
+		key=key%26;
+	if(key<0)
+	{
+		while(key<0)
+			key+=26;
+	}
+	int len=text.length();
+	string res="";
+	for(int i=0;i<len;i++)
+	{
+		if(text[i]!=' ')
+		{
+			int t=text[i]-'a';
+			int t2=(t+key)%26;
+			res+=(char)(65+t2);
+		}
+		else
+			res+=text[i];
+	}
+	return res;
+
+}
+string decrp(string text,int key)
+{
+	if(key>26)
+		key=key%26;
+	if(key<0)
+	{
+		while(key<0)
+			key+=26;
+	}
+	int len=text.length();
+	string res="";
+	for(int i=0;i<len;i++)
+	{
+		if(text[i]!=' ')
+		{
+			int t=text[i]-'A';
+			int t2=(t)%26-key;
+			if(t2<0)
+			{
+				res+=(char)(97+(26+t2));
+			}
+			else
+				res+=(char)(97+t2);
+		}
+		else
+			res+=text[i];
+	}
+	return res;
+
+}
+int main()
+{
+
+	// string text;
+	// cout<<"Enter text message\n";
+	// getline(cin,text);
+	int opt;
+	cout<<"Enter \n1) To Encrypt\n2)To Decrypt\n";
+	cin>>opt;
+	cout<<"Enter input file name\n";
+			string fname;
+			cin>>fname;
+			cout<<"Enter output file name\n";
+			string oname;
+			cin>>oname;
+			cout<<"Enter key value\n";
+			int key;
+			cin>>key;
+			ifstream fin;
+			ofstream fout;
+			fout.open(oname);
+			// if(fout.is_open())
+			// 	cout<<"true";
+			// else
+			// 	cout<<"false";
+
+			fin.open(fname);
+			// if(fin.is_open())
+			// 	cout<<"true";
+			// else
+				// cout<<"false";
+		if(opt==1)
+		{		
+		// cout<<"hello";	
+			while(fin)
+			{
+				string str;
+				getline(fin,str);
+				// cout<<str;
+				string t=encp(str,key);
+				// cout<<t<<endl;
+				fout<<t<<endl;
+			}
+			fin.close();
+			fout.close();
+		}
+		if(opt==2)
+		{
+			while(fin)
+			{
+				string str;
+				getline(fin,str);
+				string t=decrp(str,key);
+				fout<<t<<endl;
+			}	
+			fin.close();
+			fout.close();
+		}
+return 0;
+}
