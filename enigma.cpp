4ba92a8cf23fed0a0f2e70f05e510e6d1f12d3a7
Jayansh Goyal <smartjayansh@gmail.com>
2019-09-05 19:34:03 +0530

    Changes in enigma

--- enigma.cpp
+++ enigma.cpp
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+int posl, posm, posr;
+void tra(vector<char> l, vector<char> m, vector<char> r)
+{
+}
+
 string encrypt(string text)
 {
     string st = "qwertyuiopasdfghjklzxcvbnm";
     int tlen = text.length();
-    string left, mid, right, st2;
+    string left, mid, right, ref;
     left = "qazwsxedcrfvtgbyhnujmikolp";
     mid = "zxcvbnmasdfghjklqwertyuiop";
     right = "asdfghjklqzwxecrvtbynumiop";
-    st2 = "pqowieurytlaksjdhfgmznxbcv";
+    ref = "pqowieurytlaksjdhfgmznxbcv";
     // cout << left.length() << " " << mid.length() << " " << right.length();
-    vector<char> l, m, r;
+    vector<char> l, m, r, str;
+    string res = "";
+    for (int i = 0; i < st.length(); i++)
+        str.push_back(st[i]);
     for (int i = 0; i < left.length(); i++)
         l.push_back(left[i]);
 
@@ -22,30 +30,47 @@ string encrypt(string text)
 
     for (int i = 0; i < tlen; i++)
     {
-        char c = st[text[i] - 'a'];
+        char c = str[text[i] - 'a'];
+        cout << c << " ";
         char d = l[c - 'a'];
+        cout << d << " ";
         char e = m[d - 'a'];
+        cout << e << " ";
         char f = r[e - 'a'];
-        char g = st2[f - 'a'];
+        cout << f << " ";
+        char g = ref[f - 'a'];
+        cout << g << " ";
 
         //traversing back
         vector<char>::iterator it;
         int dis;
         dis = distance(r.begin(), find(r.begin(), r.end(), g));
-        char h = 'a' + d;
+        char h = 'a' + dis;
+        cout << h << " ";
+        dis = distance(m.begin(), find(m.begin(), m.end(), h));
+        char j = 'a' + dis;
+        cout << j << " ";
+        dis = distance(l.begin(), find(l.begin(), l.end(), j));
+        char k = 'a' + dis;
+        cout << k << " ";
 
-        dis = distance(r.begin(), find(r.begin(), r.end(), g));
-        char h = 'a' + d;
+        dis = distance(str.begin(), find(str.begin(), str.end(), k));
+        cout << str[dis] << " ";
 
-        dis = distance(r.begin(), find(r.begin(), r.end(), g));
-        char h = 'a' + d;
+        res += str[dis];
+
+        // rotate(l.begin(), l.begin() + 1, l.end());
+        // rotate(m.begin(),m.begin()+1,m.end());
+        // rotate(r.begin(),r.begin()+1,r.end());
+        cout << endl;
     }
+    return res;
 }
 int main()
 {
     string text, cipher;
     text = "abc";
     cipher = encrypt(text);
-
+    cout << cipher;
     return 0;
 }
